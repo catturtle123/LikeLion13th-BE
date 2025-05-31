d732ab2c628e076e089cda80282efc1ef498b4b9
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-06-01 00:49:57 +0900

    a

--- src/main/java/com/example/redisspring/BoardService.java
+++ src/main/java/com/example/redisspring/BoardService.java
@@ -17,7 +17,7 @@ public class BoardService {
 
     // cache aside 전략으로 캐싱이 적용 된다.
     // 알아서 캐시 찌르고 없으면 알아서 다시 DB로 간 다음 저장시켜준다
-    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size", cacheManager = "boardCacheManager")
+//    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size", cacheManager = "boardCacheManager")
     public List<Board> getBoards(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
         Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
